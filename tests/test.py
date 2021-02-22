80ed7933a5537a1227dab8add1988660dccb31e2
Your Name <victor.meynaud@chartes.psl.eu>
2021-02-22 10:19:04 +0100

    corrigé yaml car bug Travis CI

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,9 @@
 import glob
 # Librairie permettant d'interagir avec n'importe quel OS
 import os
-
+from os.path import dirname, abspath
 #Librairie XML
+
 from lxml import etree
 from lxml.etree import XMLSyntaxError
 
@@ -13,67 +14,73 @@ from lxml.etree import XMLSyntaxError
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
 
 # Import du document XML du dossier d'au-dessus modifié le plus récemment
-doc_a_tester = max(glob.iglob('../*.xml'), key=os.path.getctime)
+# doc_a_tester = max(glob.iglob('../*.xml'), key=os.path.getctime)
 
-# Import du schéma indépendant de l'OS
-schema = os.path.abspath(os.path.join(chemin_actuel, os.pardir, "tei_all.rng"))
+dossier = dirname(dirname(abspath(__file__)))
 
-print(schema)
+for filename in dossier:
+    if filename.endswith(".xml"):
+        print("Fichier en cours de test : {fichier}".format(fichier = filename))
 
-print(doc_a_tester)
-print(schema)
+        # Import du schéma indépendant de l'OS
+        schema = os.path.abspath(os.path.join(chemin_actuel, os.pardir, "tei_all.rng"))
 
-def test_well_formed(doc):
-    """ Test XM# doc_a_tester = os.path.abspath(os.path.join(chemin_actuel, os.pardir, "*.xml"), key=os.path.getctime)L bien formé (par rapport à la syntaxe XML)
-    
-    :param doc: doc à tester
-    :type doc: TextIOWrapper
-    :returns: rien
-    """
-    with open(doc) as doc:
-        try:
-            doc = etree.parse(doc)
-            print("Votre XML est bien formé")
-        except XMLSyntaxError:
-            print("Attention ! Votre XML n'est pas bien formé")
+        # print(schema)
 
-# Test présence du name space TEI (les atributs fonctionnent comme des dict)
-def test_ns(doc):
-    """ Test si l'élément racine est bien TEI avec le ns TEI
-    
-    :param doc: doc à tester
-    :type doc: TextIOWrapper
-    :returns: rien
-    """
-    with open(doc) as doc:
-        doc = etree.parse(doc)
-        # récupère tous les enfants de la root
-        root = doc.getroot()
-        # récupère l'élément racine
-        root = root.tag
-        assert root == "{http://www.tei-c.org/ns/1.0}TEI", "La racine doit être TEI (avec l'espace de nom TEI"
+        # print(doc_a_tester)
+        # print(schema)
 
+        def test_well_formed(doc):
+            """ Test XM# doc_a_tester = os.path.abspath(os.path.join(chemin_actuel, os.pardir, "*.xml"), key=os.path.getctime)L bien formé (par rapport à la syntaxe XML)
+            
+            :param doc: doc à tester
+            :type doc: TextIOWrapper
+            :returns: rien
+            """
+            with open(doc) as doc:
+                try:
+                    doc = etree.parse(doc)
+                    print("Votre XML est bien formé")
+                except XMLSyntaxError:
+                    print("Attention ! Votre XML n'est pas bien formé")
 
-
-# Test XML valide (par rapport au schéma du projet)
-def test_schema(doc):
-    """ Test si le document est conforme au schéma du projet 
-    
-    :param doc: doc à tester
-    :type doc: TextIOWrapper
-    :returns: rien
-    """
-    with open(doc) as doc:
-        relaxng_doc = etree.parse(schema)
-        relaxng = etree.RelaxNG(relaxng_doc)
-        doc = etree.parse(doc)
-        if relaxng.validate(doc) == True:
-           print("Votre document est conforme au schéma!")
-        else:
-            raise Exception("Votre document n'est pas valide par rapport au schéma!")
+        # Test présence du name space TEI (les atributs fonctionnent comme des dict)
+        def test_ns(doc):
+            """ Test si l'élément racine est bien TEI avec le ns TEI
+            
+            :param doc: doc à tester
+            :type doc: TextIOWrapper
+            :returns: rien
+            """
+            with open(doc) as doc:
+                doc = etree.parse(doc)
+                # récupère tous les enfants de la root
+                root = doc.getroot()
+                # récupère l'élément racine
+                root = root.tag
+                assert root == "{http://www.tei-c.org/ns/1.0}TEI", "La racine doit être TEI (avec l'espace de nom TEI"
 
 
-# On appelle les fonctions de test sur le document à tester
-test_well_formed(doc_a_tester)
-test_ns(doc_a_tester)
-test_schema(doc_a_tester)
+
+        # Test XML valide (par rapport au schéma du projet)
+        def test_schema(doc):
+            """ Test si le document est conforme au schéma du projet 
+            
+            :param doc: doc à tester
+            :type doc: TextIOWrapper
+            :returns: rien
+            """
+            with open(doc) as doc:
+                relaxng_doc = etree.parse(schema)
+                relaxng = etree.RelaxNG(relaxng_doc)
+                doc = etree.parse(doc)
+                if relaxng.validate(doc) == True:
+                print("Votre document est conforme au schéma!")
+                else:
+                    raise Exception("Votre document n'est pas valide par rapport au schéma!")
+
+
+        # On appelle les fonctions de test sur le document à tester
+        test_well_formed(doc_a_tester)
+        test_ns(doc_a_tester)
+        test_schema(doc_a_tester)
